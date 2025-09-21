5b3f60e826de04298a82664986e375b88c2cdf45
Anjali jain <rohini1jb@gmail.com>
2025-09-21 23:46:26 +0530

    done

--- xdictionary/src/App.js
+++ xdictionary/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [searchWord, setSearchWord] = useState("");
+  const [wordMeaning, setWordMeaning] = useState("");
+
+  const findInDictionary = () => {
+    setWordMeaning("");
+
+    const dictionary = [
+      {
+        word: "React",
+        meaning: "A JavaScript library for building user interfaces.",
+      },
+      { word: "Component", meaning: "A reusable building block in React." },
+      { word: "State", meaning: "An object that stores data for a component." },
+    ];
+
+    const lowerCaseSearchedWords = searchWord.toLowerCase();
+
+    for (const entry of dictionary) {
+      const lowerCaseDictionaryWords = entry.word.toLowerCase();
+      if (lowerCaseSearchedWords === lowerCaseDictionaryWords) {
+        setWordMeaning(entry.meaning);
+        return;
+      }
+    }
+
+    setWordMeaning((prevWordMeaning) => {
+      if (prevWordMeaning === "") {
+        return "Word not found in the dictionary.";
+      }
+      return prevWordMeaning;
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    findInDictionary();
+  };
+
+  const handleChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dictionary App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a word..."
+          value={searchWord}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Definition:</h3>
+      <p>{wordMeaning}</p>
     </div>
   );
-}
+};
 
 export default App;
